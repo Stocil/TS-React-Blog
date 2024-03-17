f6788747774e86fb4a042630263443a5d82d007b
Stocil <vashurov02@mail.ru>
2024-03-17 04:17:34 +0300

    feat: add form validation

--- src/pages/AccountManager/AccountManager.tsx
+++ src/pages/AccountManager/AccountManager.tsx
@@ -1,18 +1,52 @@
-import { FC } from "react";
-import { Button, Container, Stack, TextField } from "@mui/material";
+import { FC, useState } from "react";
+import {
+  Button,
+  Container,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import { useLocation } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
 import { FormWrapper } from "./AccountManager.styles.tsx";
 import GradientText from "../../components/UIkit/GradientText";
+import { Visibility } from "@mui/icons-material";
 
-type AuthFields = {
-  name: string;
+type InputFields = {
+  name: "username" | "email" | "password" | "repeatPassword";
   id: string;
   label: string;
   type?: string;
 };
 
+type FormInputs = {
+  username: string;
+  email?: string;
+  password: string;
+  repeatPassword?: string;
+};
+
+// TODO Full refactor!!
+
 const AccountManager: FC = () => {
   const path = useLocation().pathname;
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isValid },
+  } = useForm<FormInputs>({ mode: "onBlur" });
+
+  console.log(errors);
 
   const renderTitle = () => {
     if (path === "/sign-in") {
@@ -31,12 +65,12 @@ const AccountManager: FC = () => {
   };
 
   const renderFields = () => {
-    let fields: AuthFields[] = [];
+    let fields: InputFields[] = [];
 
     if (path === "/sign-in") {
       fields = [
         {
-          name: "userName",
+          name: "username",
           id: "username",
           label: "Username",
           type: "text",
@@ -45,6 +79,7 @@ const AccountManager: FC = () => {
           name: "password",
           id: "password",
           label: "Password",
+          type: "password",
         },
       ];
     }
@@ -52,7 +87,7 @@ const AccountManager: FC = () => {
     if (path === "/sign-up") {
       fields = [
         {
-          name: "userName",
+          name: "username",
           id: "username",
           label: "Username",
           type: "text",
@@ -67,30 +102,98 @@ const AccountManager: FC = () => {
           name: "password",
           id: "password",
           label: "Password",
+          type: "password",
         },
         {
           name: "repeatPassword",
           id: "repeatPassword",
           label: "Repeat Password",
+          type: "password",
         },
       ];
     }
 
     return fields.map((field) => {
+      if (field.type === "password") {
+        return (
+          <FormControl key={field.name} sx={{ width: 1 }} variant="outlined">
+            <InputLabel
+              color={errors[field.name]?.message ? "error" : "primary"}
+              htmlFor={field.name}>
+              {field.label}
+            </InputLabel>
+
+            <OutlinedInput
+              error={!!errors[field.name]?.message}
+              type={isShowPassword ? "text" : "password"}
+              id={field.id}
+              label={field.label}
+              autoComplete="off"
+              fullWidth
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setIsShowPassword((show) => !show)}
+                    onMouseDown={(e) => e.preventDefault()}
+                    edge="end">
+                    {isShowPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              {...register(field.name, {
+                required: "Password is required",
+                minLength: {
+                  value: 5,
+                  message: "Password is too short",
+                },
+                validate: {
+                  isMatch: (value) => {
+                    if (field.name === "repeatPassword") {
+                      return (
+                        value === watch("password") || "Passwords do not match"
+                      );
+                    }
+                  },
+                },
+              })}
+            />
+
+            {errors[field.name] ? (
+              <Typography variant="body2" color="error">
+                {errors[field.name]?.message}
+              </Typography>
+            ) : null}
+          </FormControl>
+        );
+      }
+
       return (
         <TextField
           key={field.name}
-          name={field.name}
           id={field.id}
-          label={field.label}
           type={field.type}
+          label={field.label}
           variant={"outlined"}
           autoComplete="off"
           required
           fullWidth
+          {...register(field.name, {
+            required: `${field.label} is required`,
+            minLength: {
+              value: 4,
+              message: `${field.label} is too short`,
+            },
+          })}
+          error={!!errors[field.name]}
+          helperText={errors[field.name]?.message}
         />
       );
     });
+  };
+
+  const onSubmit = (data: FormInputs) => {
+    console.log(data);
   };
 
   return (
@@ -106,10 +209,10 @@ const AccountManager: FC = () => {
             {renderTitle()}
           </GradientText>
 
-          <form className="auth__form">
+          <form className="auth__form" onSubmit={handleSubmit(onSubmit)}>
             {renderFields()}
 
-            <Button type="submit" size="large">
+            <Button type="submit" size="large" disabled={!isValid}>
               {renderButtonText()}
             </Button>
           </form>
