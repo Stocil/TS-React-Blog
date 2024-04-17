fbff63e97c3e11db3469effde86daf46e4ad1531
Stocil <vashurov02@mail.ru>
2024-04-17 23:30:13 +0300

    add: default light theme scheme

--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -31,7 +31,7 @@ declare module "@mui/material/Button" {
 export function Theme({ children }: ThemeProps) {
   const mode: PaletteMode = useTypedSelector((state) => state.theme.theme);
 
-  const theme = createTheme({
+  let theme = createTheme({
     palette: {
       mode,
       primary: {
@@ -91,6 +91,69 @@ export function Theme({ children }: ThemeProps) {
       },
     },
   });
+
+  if (mode === "light") {
+    theme = createTheme({
+      palette: {
+        mode,
+        primary: {
+          main: indigo[200],
+        },
+        secondary: {
+          main: "#eda7f1",
+        },
+        dark: {
+          light: "#2D2D2D",
+          main: "#191919",
+        },
+        background: {
+          // default: "#171717",
+          // paper: "#171717",
+          default: "#1a1d24",
+          paper: "#1a1d24",
+        },
+      },
+
+      typography: {
+        fontFamily: "'Inter', sans-serif",
+      },
+      shape: {
+        borderRadius: 10,
+      },
+
+      components: {
+        MuiContainer: {
+          defaultProps: {
+            component: "section",
+            maxWidth: "lg",
+          },
+        },
+        MuiButton: {
+          defaultProps: {
+            variant: "contained",
+          },
+          variants: [
+            {
+              props: { variant: "gradient" },
+              style: ({ theme }) => ({
+                backgroundImage: `linear-gradient(to right, ${theme.palette.primary.main} 0%, #eda7f1  51%, ${theme.palette.primary.main}  100%)`,
+                transition: "0.5s",
+                backgroundSize: "200% auto",
+                ":hover": {
+                  backgroundPosition: "right center",
+                },
+              }),
+            },
+          ],
+          styleOverrides: {
+            root: {
+              textTransform: "none",
+            },
+          },
+        },
+      },
+    });
+  }
 
   return (
     <ThemeProvider theme={theme}>
